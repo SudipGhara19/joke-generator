9358ea6d9ba136d3789594cc3d023dc4a80a85a5
SudipGhara19 <sudipghara135@gmail.com>
2024-01-04 20:18:37 +0530

    implemented -useFetch- Hook in the App.js successfully

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import "./styles.css";
 // import the custom hook to use in this document
+import useFetch from "./useFetch";
+
+
 export default function App() {
   const url = "https://v2.jokeapi.dev/joke/Programming?type=single";
+
   // Use the custom hook here
+  const {data, loading, error, getJoke} = useFetch(url);
+  
 
   // Display loading text here
+  if(loading){
+    return(
+      <div className="App">
+        <h1>Joke Generator</h1>
+        <p>Loading...</p>
+      </div>
+    )
+  }
 
   // Display something went wrong here
+  if(error){
+    return(
+      <div className="App">
+        <h1>Joke Generator</h1>
+        <p>Something went wrong: {error}</p>
+      </div>
+    )
+  }
   
   return (
     <div className="App">
       <h1>Joke Generator</h1>
       {/* Do not modify the below code */}
       <h2>{data.joke}</h2>
-      <button className="btn" >New Joke</button>
+      <button className="btn" onClick={getJoke}>New Joke</button>
     </div>
   );
 }
